b6cbc9011444e3fdf0b6f14418a33c9724cd8ce0
DESKTOP-ETR292H\Rebecca <hovemeyer5@marshall.edu>
2017-06-20 15:21:44 -0400

    Add hero service to app component
    - added import statement
    - added to provisions
    - added getHeroes function to call hero service one
    - added OnInit Template

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './services/hero.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   heroes: Hero[];
 
+  constructor(private heroService: HeroService) { }
+
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
   }
+
+  getHeroes(): void{
+  	this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+  	this.getHeroes();
+	}
 }
