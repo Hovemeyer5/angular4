1993607a3dadab2a5ea31ba7dd298acfff53b4bc
DESKTOP-ETR292H\Rebecca <hovemeyer5@marshall.edu>
2017-06-20 15:13:37 -0400

    Create hero service
    - Create hero service file
    - Create mock hero service return 
    - remove mock hero array from app

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,20 +1,6 @@
 import { Component } from '@angular/core';
-export class Hero {
-	id: number;
-	name: string;
-}
-const HEROES: Hero[] = [  
-	{ id: 11, name: 'Mr. Nice' },  
-	{ id: 12, name: 'Narco' },  
-	{ id: 13, name: 'Bombasto' },  
-	{ id: 14, name: 'Celeritas' }, 
-	 { id: 15, name: 'Magneta' },  
-	 { id: 16, name: 'RubberMan' },  
-	 { id: 17, name: 'Dynama' },  
-	 { id: 18, name: 'Dr IQ' },  
-	 { id: 19, name: 'Magma' },  
-	 { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+
 
 @Component({
   selector: 'app-root',
@@ -24,7 +10,7 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
