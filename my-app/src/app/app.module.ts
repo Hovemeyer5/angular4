c6150a32fdd3607b50e91268fbc2baceeefc37bb
DESKTOP-ETR292H\Rebecca <hovemeyer5@marshall.edu>
2017-06-21 08:14:19 -0400

    Moving Routing to routing module

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
 
 import { HeroDetailComponent } from './heroDetail/hero-detail.component';
 import {HeroesComponent} from './myHeroes/heroes.component';
 import { AppComponent } from './app.component';
 import { HeroService }         from './services/hero.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -20,25 +20,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule
   ],
   providers: [
     HeroService
