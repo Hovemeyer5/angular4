503811a84029c61f2a81744c9289bcbeb42dcba9
DESKTOP-ETR292H\Rebecca <hovemeyer5@marshall.edu>
2017-06-21 11:22:48 -0400

    Pulling from local "mock" api

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { HeroDetailComponent } from './heroDetail/hero-detail.component';
 import {HeroesComponent} from './myHeroes/heroes.component';
@@ -9,6 +10,9 @@ import { HeroService }         from './services/hero.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
 
 import { AppRoutingModule }     from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,6 +24,8 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [
