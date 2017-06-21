1284546cb45db37f37c68e88d9b0992d0002f1e1
DESKTOP-ETR292H\Rebecca <hovemeyer5@marshall.edu>
2017-06-21 07:44:59 -0400

    Add Routing;

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,23 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 import { HeroDetailComponent } from './heroDetail/hero-detail.component';
 import {HeroesComponent} from './myHeroes/heroes.component';
 import { AppComponent } from './app.component';
+import { HeroService }         from './services/hero.service';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
